3ef794678e53a6f680e348814e0b484a11a062dd
Raja Vikram <rajavikram@zemosolabs.com>
2017-02-09 12:20:36 +0530

    Include Microphone recording. Build No: 0.90.2

--- InAppText SDK Demo/AppDelegate.swift
+++ InAppText SDK Demo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        InAppText.instantiateSDK(true)
+        InAppText.instantiateSDK(true,microphoneEnabled: true)
         return true
     }
 
